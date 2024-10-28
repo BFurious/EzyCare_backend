118ddbdfbd05caa0dd01c40c8ec2c549029d1d32
Ashutosh thakur <ashutoshth456@gmail.com>
2024-10-29 02:29:14 +0530

    video call

--- src/server.ts
+++ src/server.ts
@@ -114,6 +114,18 @@ async function bootstrap() {
       }
     })
 
+    socket.on('offer', (data) => {
+      socket.to(data.roomId).emit('offer', data.offer);
+    });
+  
+    socket.on('answer', (data) => {
+      socket.to(data.roomId).emit('answer', data.answer);
+    });
+  
+    socket.on('ice-candidate', (data) => {
+      socket.to(data.roomId).emit('ice-candidate', data.candidate);
+    });  
+
     // Handle user disconnecting
     socket.on('disconnect', (reason) => {
         const roomId = Object.keys(rooms).find((id) => {
