69affabb72d50f7698afb0a8d4e5c006f3967b05
Aa9873284320@ <ashutoshth456@gmail.com>
2024-11-30 00:26:41 +0530

    optmize and bug

--- src/helpers/Transporter.ts
+++ src/helpers/Transporter.ts
@@ -1,31 +1,6 @@
 import nodemailer from 'nodemailer';
 import { google } from 'googleapis';
 import config from '../config';
-const OAuth2 = google.auth.OAuth2;
-
-
-// const oauth2Client = new OAuth2(
-//     '265605923744-mt0so60cf5ofosjfjo3orsqlpib8tbgs.apps.googleusercontent.com',
-//     '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',
-//     'YOUR_REDIRECT_URL'
-// );
-
-// oauth2Client.setCredentials({
-//     refresh_token: 'YOUR_REFRESH_TOKEN',
-// });
-// const accessToken : any = oauth2Client.getAccessToken();
-
-// export const Transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         type: 'OAuth2',
-//         user: 'your-email@gmail.com',
-//         clientId: '265605923744-mt0so60cf5ofosjfjo3orsqlpib8tbgs.apps.googleusercontent.com',
-//         clientSecret: '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',
-//         refreshToken: 'YOUR_REFRESH_TOKEN',
-//         accessToken: accessToken.token!,
-//     },
-// });
 
 export const Transporter = nodemailer.createTransport({
     service: 'gmail',
